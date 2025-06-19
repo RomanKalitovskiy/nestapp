cb217267fd3da7b73d2375d65491ff2dd10f19f6
RomanKalitovskiy <roman.kalitovskiy@gmail.com>
2025-06-19 16:59:48 +0300

    Update PostgreSQL configuration and dependencies. Added 'pg' package version 8.16.1 and updated data source to use DATABASE_URL for production settings.

--- src/data-source.ts
+++ src/data-source.ts
@@ -22,13 +22,10 @@ if (isTest) {
 } else if (isProd) {
   dataSourceOptions = {
     type: 'postgres',
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-    host: process.env.DB_HOST,
+    url: process.env.DATABASE_URL,
     entities: [User, Report],
     migrations: [`migrations/*.${migrationExt}`],
+    migrationsRun: true,
     synchronize: false,
   } as DataSourceOptions;
 } else {
